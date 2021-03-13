9e8236cbdb0442e644568f31710afc2b8b354043
CauaMatheus <caua10000@gmail.com>
2021-03-13 12:17:32 -0300

    VerifyToken Middleware

---
+++ src/middlewares/VerifyIfIsAnValidToken.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import dotEnv from '../.env';
+
+interface TokenPayload {
+  _id: string
+  id: string,
+  iat: number,
+  exp: number
+}
+
+class VerifyIfIsAnValidToken {
+  async execute(request: Request, response: Response, next: NextFunction) {
+    const { authorization } = request.headers;
+    if (!authorization) {
+      return response.status(404).json({ message: 'You need to insert an authorization token' });
+    }
+
+    try {
+      const { _id } = jwt.verify(authorization, dotEnv.secrectKey) as TokenPayload;
+      request.userId = _id;
+      return next();
+    } catch {
+      return response.status(404).json({ message: 'Invalid token' });
+    }
+  }
+}
+const verifyIfIsAnValidToken = new VerifyIfIsAnValidToken();
+export default verifyIfIsAnValidToken;
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import userController from './controllers/Users';
+import verifyIfIsAnValidToken from './middlewares/VerifyIfIsAnValidToken';
 
 const router = express.Router();
 
 router.post('/signup', userController.create);
 router.post('/signin', userController.authenticate);
 router.get('/users/:id', userController.findUser);
+
+router.use(verifyIfIsAnValidToken.execute);
 
 export default router;
